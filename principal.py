b4a88729da1e70aa0d900c896f70213a1d935fda
Julieth <juliethrojas755@gmail.com>
2026-02-14 22:12:08 -0500

    Submenu inscripcion del camper

--- principal.py
+++ principal.py
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
-def menu_principal ():
-    while True:
-      print ("=======CAMPUSLANDS===========")
-      print ("1. Camper")
-      print ("2. Trainer ")
-      print ("3. Coordinador")
 
-      opcion = input("Seleccione una opcion:")  
-
-      match opcion:
-         
-         case 1:
-            print("Entrando al menu del camper")
-         case 2: 
-            print("Entrando al menu del trainer ")
-         case 3:
-            print("Entrando al menu del coordinador")
-
-def  menu_camper ():
-=======
-#=================================
-#=== MENU INSCRIPCION CAMPER=====
-#=================================
 
 def  menu_inscripcion ():
->>>>>>> 72a1455 (Submenu inscripcion del camper)
+
    while True:
       print ("n|=======INSCRIPCION============")
       print ("1. Ingreso de identidad ")
@@ -59,23 +36,4 @@ def menu_trainer ():
      print ("4. Volver")
    
      opcion = input ("Seleccione una opcion:")
-    
-     match opcion:
-        case 1:
-<<<<<<< HEAD
-         print
-       #sfsfsdfs
->>>>>>> d90d2eb264fa678a03541df21feb29e2bd5545f4
-=======
-         print ("Esta es tu ruta asignada ")
-        case 2:
-         print ("Registro de notas...")
-        case 3: 
-         print ("Campers asignados")
-        case 4:
-         break
-        case _:
-         print ("Opcion invalida")
-             
-       
->>>>>>> 72a1455 (Submenu inscripcion del camper)
+     
