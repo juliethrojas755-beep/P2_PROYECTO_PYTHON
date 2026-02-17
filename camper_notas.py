05a9f5acfb158af2110e883e8377cf9b4ab9194e
Julieth <juliethrojas755@gmail.com>
2026-02-17 07:52:45 -0500

    actualizacion_campers_notas

--- camper_notas.py
+++ camper_notas.py
@@ -1,15 +1,15 @@
 
-import json
+from json_utils import leer_json
+
+
+
 
 def leer_campers():
-    try:
-        with open("campers.json", "r") as f:
-            return json.load(f)
-    except:
-        return []
+     return leer_json("campers.json")
 
 def buscar_camper(id_camper):
-    for c in leer_campers():
+    campers = leer_campers
+    for c in campers:
         if c["IDcamper"] == id_camper:
             return c
     return None
@@ -25,7 +25,7 @@ def menu_ver_notas(id_camper):
 
     
     if estado in ["en proceso de ingreso", "inscrito", "aprobado"]:
-        print("⚠️ Aún no tiene notas. Su proceso formativo no ha iniciado.")
+        print(" Aún no tiene notas. Su proceso formativo no ha iniciado.")
         return
 
     if estado in ["expulsado", "retirado"]:
@@ -37,7 +37,7 @@ def menu_ver_notas(id_camper):
         return
 
     while True:
-        print("\n---- VER NOTAS ----")
+        print("---- VER NOTAS ----")
         print("1. Notas generales")
         print("2. Notas por módulo")
         print("3. Regresar")
@@ -60,16 +60,16 @@ def ver_notas_generales(camper):
     print("\n NOTAS GENERALES")
     for modulo, notas in camper["modulos"].items():
         print(f"\n {modulo}")
-        for k, v in notas.items():
-            print(f"  {k}: {v}")
+        for  tipo,valor in notas.items():
+            print(f"  {tipo}: {valor}")
 
 def ver_notas_por_modulo(camper):
     modulo = input("Ingrese el módulo: ").lower()
-
-    if modulo in camper["modulos"]:
+    modulos = camper.get ("modulos",{})
+    if modulo in modulos:
         print(f"\n {modulo}")
-        for k, v in camper["modulos"][modulo].items():
-            print(f"  {k}: {v}")
+        for tipo, valor in modulos[modulo].items():
+            print(f"  {tipo}: {valor}")
     else:
         print(" Módulo no encontrado")
 
